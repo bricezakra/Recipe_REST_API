1cec554459e5d7fe2d88d7c3f8b6d53cdea1720b
bricezakra <bricezakra@live.com>
2020-01-06 21:07:42 -0600

    autopep8 installed

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,7 +8,7 @@ class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that two numbers are add`ed together"""
         self.assertEqual(add(3, 8), 11)
-    
+
     def test_subtract_numbers(self):
         """Test that values are subtracted and returned"""
         self.assertEqual(subtract(5, 12), 7)
